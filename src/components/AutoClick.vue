39a23111f97f540f4c156053f2c69629c10cc591
ChChEch <christianchidi.echere@tptlive.ee>
2021-10-13 00:10:47 +0300

    click

--- src/components/AutoClick.vue
+++ src/components/AutoClick.vue
@@ -1,20 +1,26 @@
 <template>
-  <button class="button is-warning" @click="autoClick" :disabled="clicks<cost">
-      {{name}} ({{cps}} clicks per second) cost: {{cost}} clicks
+  <button class="button is-danger" @click="autoClick" :disabled="clicks<cost">
+      {{name}} (increases deletion per click by {{clickrate}} and {{cps}} deletes per second) cost: {{cost}} deletes
   </button>
 </template>
 
 <script>
 export default {
-    props: ['cost', 'cps', 'name', 'clicks', 'index'],
+    props: ['cost', 'clickrate', 'cps', 'name', 'clicks', 'index'],
     methods: {
         autoClick(){
             this.$emit('clicked', this.index);
-        }
+        },
+        summoner() {
+            this.$emit('clicked', this.index);
+        },
     }
 }
 </script>
 
 <style>
-
+.button.is-danger{
+    margin-right: 10px;
+    margin-top: 10px;
+}
 </style>
--- src/components/ClickerExample.vue
+++ src/components/ClickerExample.vue
@@ -1,7 +1,11 @@
 <template>
   <section class="section">
-        <button class="button is-danger" @click="clicks++">Click me!</button>
-        <h4 class="is-size-4">You have {{displayClicks}} clicks!</h4>  
+        <button @click="do_click" @click.prevent="playSound('https://www.101soundboards.com/sounds/643197-audio-appearance.mp3')">
+            <img src="https://miro.medium.com/max/1292/1*R6L2ogI6dYUEMlFZVzlnnA.png" width="200px">
+            
+        </button>
+        <h4 class="is-size-4">You have deleted {{displayClicks}} accounts!</h4>
+        <h5 class="is-size-3">You have a lot of Deleting to do!</h5>  
         <auto-click 
         v-for="(auto,index) in autoClickers"
         :key="index"
@@ -9,16 +13,32 @@
         :cps="auto.cps"
         :cost="auto.cost"
         :index="index"
+        :clickrate="auto.clickrate"
+        :summoner="summoner"
+        :clicks="clicks"
+        @clicked="autoClick"
+        >
+        </auto-click>
+        <summoner
+        v-for="(auto,index) in summoner"
+        :key="index"
+        :name="auto.name"
+        :cps="auto.cps"
+        :cost="auto.cost"
+        :index="index"
+        :clickrate="auto.clickrate"
+        :summoner="summoner"
         :clicks="clicks"
         @clicked="autoClick">
-        </auto-click>
+        </summoner>
   </section>
 </template>
 
 <script>
+import Clickrate from './Clickrate.vue';
 import AutoClick from './AutoClick.vue';
 export default {
-  components: { AutoClick },
+  components: { AutoClick, Clickrate },
     mounted(){
         setInterval(()=>{
             this.clicks += this.cps;   
@@ -26,14 +46,28 @@ export default {
     },
     data(){
         return {
-            clicks: 0,
+            clicks: 1000,
+            clickrate: 1,
             cps: 0,
             cost: 10,
             autoClickers: [
-                {name: 'Auto Click', cost: 10, cps:0.1},
-                {name: 'Grandma', cost: 100, cps:1},
-                {name: 'Factory', cost: 1000, cps:10},
-                {name: 'Ultron', cost: 10000, cps:100},
+                {name: 'Auto Delete', cost: 10, clickrate: 0, cps:0.1},
+                {name: 'Delete Bot', cost: 100, clickrate: 0, cps:1},
+                {name: 'Factory of Deleters', cost: 1000, clickrate: 0, cps:10},
+                {name: 'Entire City of Deleters', cost: 10000, clickrate: 0, cps:100},
+                {name: 'Entire Country of Deleters', cost: 100000, clickrate: 0, cps:1000},
+                {name: 'Entire Continent of Deleters', cost: 1000000, clickrate: 0, cps:10000},
+                {name: 'Entire World of Deleters', cost: 10000000, clickrate: 0, cps:100000},
+                {name: 'Entire Universe of Deleters', cost: 100000000, clickrate: 0, cps:1000000},
+                {name: 'Entire Multiverse of Deleters', cost: 1000000000, clickrate: 0, cps:10000000},
+                {name: 'God of Deleters', cost: 10000000000, clickrate: 0, cps:100000000},
+                {name: 'Mother of God of Deleters', cost: 100000000000, clickrate: 0, cps:1000000000},
+                {name: 'Your Angry Mother', cost: 1000000000000, clickrate: 0, cps:10000000000},
+                {name: 'Stronger Clicks', cost: 100, clickrate: 0.2, cps: 0.2},
+                ],
+        
+            summoner: [
+                {name: 'Summoner style', cost: 100, clickrate: -0.5, cps: 3}
             ]
         }
     },
@@ -41,17 +75,32 @@ export default {
         autoClick(index){
            this.clicks -= this.autoClickers[index].cost; 
            this.cps += this.autoClickers[index].cps;
+           this.clickrate += this.autoClickers[index].clickrate;
            this.autoClickers[index].cost += Math.ceil(this.autoClickers[index].cost/7);
-        }
+        },
+        do_click: function() {
+           this.clicks += this.clickrate;
+        },
     },
     computed: {
         displayClicks(){
             return this.clicks.toFixed(1);
-        }
+        },
     }
 }
 </script>
 
 <style>
-
+h4.is-size-4{
+    top:1px;
+    position: relative;
+    color:rgb(0, 0, 255);
+    border: 2px solid rgb(231, 198, 198);
+    
+}
+h5.is-size-3{
+    top:100px;
+    position:absolute;
+    color:red;
+}
 </style>
